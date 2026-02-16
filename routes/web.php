d12f8aa723b82e900cb244f8b9724106e9210def
miguelnkono <nkonondememiguel7@gmail.com>
2026-02-16 19:21:51 +0100

    added models and controllers into my application

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn() => view('home'));
+Route::get('/', [ChirpController::class, 'index']);
